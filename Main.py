f3cc707b34f693939660e882aa0221152fd9c03c
IRYfan <yifanpei98@gmail.com>
2022-11-27 14:07:54 +0000

    Learning Tkinter and PySimpleGUI

--- Main.py
+++ Main.py
@@ -1,4 +1,19 @@
 from tkinter import *
 from tkinter import ttk
+import PySimpleGUI as sg
 
-print("hello world")
+layout = [[sg.Text('My first one-shot window')],
+          [sg.InputText(), sg.InputText()],
+          [sg.Submit(), sg.Cancel()],
+          ]
+
+window = sg.Window('Window Title', layout)
+
+event, values = window.read()
+window.close()
+
+text_input = values[0]
+sg.popup('You entered', text_input)
+
+# Use PySimpleGUI to create a similar programm like the first java program
+# progress bar?
